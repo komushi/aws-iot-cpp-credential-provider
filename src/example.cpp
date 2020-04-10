62fc488fbdd8aae39899df3ca941847ef6609f24
Xu, Lei <lleixu@amazon.co.jp>
2020-04-10 17:02:13 +0900

    credential provider tested

--- src/example.cpp
+++ src/example.cpp
@@ -9,14 +9,42 @@
 #include <curlpp/Exception.hpp>
 
 
+namespace
+{
+	std::string url("");
+	std::string certFile("");
+	std::string keyName("");
+	std::string thingName("");
+}
+
 int main(int argc, char *argv[])
 {
 
-	std::string url("https://<your_credentials_provider_endpoint>/role-aliases/<your_role_alias>/credentials");
-	std::string certFile("testcert.pem");
-	std::string certType("PEM");
-	std::string keyName("testkey.pem");
-	std::string keyType("PEM");
+	if(argc == 5)
+	{
+		url = argv[1];
+		thingName = argv[2];
+		certFile = argv[3];
+		keyName = argv[4];		
+	}
+	else if(argc == 3)
+	{
+		url = argv[1];
+		thingName = argv[2];
+		certFile = "./cert/certificate.pem.crt";
+		keyName = "./cert/private.pem.key";
+	}
+	else 
+	{
+		std::cerr << "Missing argument" << std::endl 
+			<< "Usage: iotcred <credentials_provider_endpoint> <thingName> <certFile> <keyName>" << std::endl 
+			<< "Usage: iotcred <credentials_provider_endpoint> <thingName>"
+			<< std::endl;
+		return EXIT_FAILURE;
+	}
+
+	std::string thingHeader("x-amzn-iot-thingname: ");
+	thingHeader += thingName;
 
 	try
 	{
@@ -25,15 +53,15 @@ int main(int argc, char *argv[])
 		std::ostringstream os;
 
 		std::list<std::string> headers;
-		headers.push_back("x-amzn-iot-thingname: <your_thing_name>"); 
+		headers.push_back(thingHeader); 
 
 		using namespace curlpp::Options;
 		request.setOpt(new HttpHeader(headers));
 		request.setOpt(new Url(url));
 		request.setOpt(new SslCert(certFile));
-		request.setOpt(new SslCertType(certType));
+		request.setOpt(new SslCertType("PEM"));
 		request.setOpt(new SslKey(keyName));
-		request.setOpt(new SslKeyType(keyType));
+		request.setOpt(new SslKeyType("PEM"));
 
 		request.perform();
 
